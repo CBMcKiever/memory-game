fe9f6311b9262aeb816570a026817f5df3b3b041
Cole McKiever <colemckiever@gmail.com>
2023-08-08 20:46:02 -0400

    Added audio

--- simon.js
+++ simon.js
@@ -4,7 +4,15 @@ const orange = document.getElementById("orange");
 const blue = document.getElementById("blue");
 
 const colors = ["green", "red", "orange", "blue"];
+
+const audioBlue = new Audio("assets/B_FLAT_BLUE.mp3");
+const audioGreen = new Audio("assets/B_FLAT_GREEN.mp3");
+const audioYellow = new Audio("assets/C_SHARP_YELLOW.mp3");
+const audioRed = new Audio("assets/F_RED.mp3");
+const playButton = document.querySelector("#play");
+
 let answers = [];
+let score = 0;
 
 const pushNextAnswer = (answers) => {
   return [...answers, colors[Math.floor(Math.random() * 4)]];
@@ -24,6 +32,7 @@ const waitForSelection = (elementId) => {
           .getElementById(color)
           .removeEventListener("click", clickHandler);
       });
+      playSound(event.target.id);
       await illuminateElement(event.target.id);
       event.target.id === elementId ? resolve(true) : resolve(false);
     }
@@ -32,6 +41,37 @@ const waitForSelection = (elementId) => {
       document.getElementById(color).addEventListener("click", clickHandler);
     });
   });
+};
+
+const playSound = (color) => {
+  pauseAndRestartAudio();
+  switch (color) {
+    case "blue":
+      audioBlue.play();
+      break;
+    case "green":
+      audioGreen.play();
+      break;
+    case "orange":
+      audioYellow.play();
+      break;
+    case "red":
+      audioRed.play();
+      break;
+    default:
+      return;
+  }
+};
+
+const pauseAndRestartAudio = () => {
+  audioBlue.pause();
+  audioBlue.currentTime = 0;
+  audioGreen.pause();
+  audioGreen.currentTime = 0;
+  audioRed.pause();
+  audioRed.currentTime = 0;
+  audioYellow.pause();
+  audioYellow.currentTime = 0;
 };
 
 const illuminateElement = async (id) => {
@@ -57,17 +97,19 @@ async function gameLoop() {
   mainLoop: while (true) {
     answers = pushNextAnswer(answers);
     console.log(answers);
-    showAnswers(answers);
+    await showAnswers(answers);
     for (let answer of answers) {
       const isSelectionCorrect = await waitForSelection(answer);
       if (isSelectionCorrect) {
         console.log("correct guess... next");
+        // console.log(++score);
+
         continue;
       } else {
         break mainLoop;
       }
     }
-    console.log("Correct!");
+    document.getElementById("score").innerHTML = ++score;
   }
   console.log("Damn dude, game over.... Loser");
 }
